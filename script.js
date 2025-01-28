1e281d075ccd80aae38387fa334c950ec5b9d3a2
shatee-ak <abithe99@gmail.com>
2025-01-28 12:28:22 +0600

    variable

---
+++ script.js
@@ -0,0 +1,62 @@
+book=60
+pen=20
+console.log(book+pen);
+
+dress=1700
+shoes=300
+vat=5
+delivery=120
+discount=15
+console.log(dress+shoes+vat+delivery-discount);
+
+food=5000
+drinks=4400
+vat=20
+discount=15%
+console.log(food+drinks+vat*discount);
+JJ=90
+EE=70
+CC=60
+console.log(JJ/CC);
+
+d=222000
+rr=5900
+console.log(d/rr);
+
+const x=70
+const y=30
+result=x+y;
+console.log(result);
+
+// Submation
+let digit1 =3900
+let digit2 =4500
+let sum =digit1+digit2;
+console . log(sum);
+
+// subtraction
+let number1=400
+let number2=200
+let num=number1-number2;
+console.log(number1-number2);
+
+// Multiplication
+let div1=300
+let div2=5
+let mul= div1*div2;
+console.log(mul);
+
+//division
+let digit4=800
+let digit3= 2
+let div= digit1/digit2;
+console.log(digit4/digit3); 
+//Remainder
+let data1=3
+let data2=2
+let remainder=data1%data2;
+console.log(remainder);
+
+
+
+
